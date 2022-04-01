6d0f4d091161c28dbe6adb257d0e0995d5a539bc
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-04-01 13:05:26 -0300

    Aula numero 3 de React da DevMedia

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import './App.css';
-
-import Header from './componentes/Header';
-import Footer from './componentes/Footer';
-import HomePage from './componentes/HomePage';
+import PaginaInicial from './componentes/PaginaInicial';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <HomePage/>
-      <Footer/>
+      <PaginaInicial/>
     </div>
   );
 }
